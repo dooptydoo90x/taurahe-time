8e16985b2f90b6e2a20ec2d4be34c772dff38d0c
Doopty Doo <sometimesidev@proton.me>
2026-04-12 20:14:37 -0500

    dwarf support

--- Taurahe_Time/Taurahe_Time.lua
+++ Taurahe_Time/Taurahe_Time.lua
@@ -1,6 +1,9 @@
 function SetTaurahe()
     local _, r = UnitRace("Player")
-    if r == "Tauren" then
+    r = r:upper()
+    if r == "TAUREN" then
         DEFAULT_CHAT_FRAME.editBox.languageID = 3
+    else if r == "DWARF"
+        DEFAULT_CHAT_FRAME.editBox.languageID = 7
     end
 end
