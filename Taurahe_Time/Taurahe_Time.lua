5f27be688c614d9299c3384f645acad7035cc106
Doopty Doo <sometimesidev@proton.me>
2026-04-16 18:03:03 -0500

    Night Elf support

--- Taurahe_Time/Taurahe_Time.lua
+++ Taurahe_Time/Taurahe_Time.lua
@@ -5,5 +5,7 @@ function SetTaurahe()
         DEFAULT_CHAT_FRAME.editBox.languageID = 3
     elseif r == "DWARF" then
         DEFAULT_CHAT_FRAME.editBox.languageID = 6
+    elseif r == "NIGHTELF" then
+        DEFAULT_CHAT_FRAME.editBox.languageID = 2
     end
 end
